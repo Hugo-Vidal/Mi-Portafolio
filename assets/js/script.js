611168a2711e76cf9375307a5ebc02abe2634807
Hugo-Vidal <hugo.f.vidal.r@gmail.com>
2019-03-26 21:23:31 -0300

    smooth scroll e img.jpeg

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,20 @@
-$(function (){
-  
-  $('[data-toggle="tooltip"]').tooltip()
+$(function(){
+
+  $("a").click(function(event){
+    if (this.hash !== "") { //si this (a.hash) es no identico a nada, ejecuta lo siguiente
+      event.preventDefault(); //Con esto, prevenimos lo que hacia antes.
+
+      var gato = this.hash;
+
+      $("html, body").animate({
+        scrollTop: $(gato).offset().top
+      }, 800, function(){
+        window.location.hash = gato;
+      });
+    }
+  });
+
+  $('[data-toggle="tooltip"]').tooltip();
 
 });
- 
 
